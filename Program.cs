5eed719428bd04980ad9c0ebb6a7013fc8e8b387
Ruthikreddy <ruthikreddy15@gmail.com>
2020-10-06 19:52:48 +0530

    adding all UC1 file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program ");
+            string FirstName;
+            String namePattern;
+            while(true)
+            {
+                namePattern = @"([A-Z]+)[a-zA-Z]{2,}";
+                Console.WriteLine("Enter your First Name with First letter in Capitals ");
+                FirstName = Console.ReadLine();
+                Regex regex = new Regex(namePattern);
+                if(regex.IsMatch(FirstName))
+                {
+                    Console.WriteLine("Valid First Name");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid First Name");
+                }
+            }
+            }
+        }
+    }
+
